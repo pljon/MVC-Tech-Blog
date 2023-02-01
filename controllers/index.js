92417bc44f055c1a775c121e38a35ca359358539
Jonathan Plaras <jonplaras@gmail.com>
2023-01-31 23:41:30 -0500

    fixed main index to go with rename file changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homepageRoutes = require('./homepageRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+const dashRoutes = require("./dashRoutes");
 
-router.use('/', homepageRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
